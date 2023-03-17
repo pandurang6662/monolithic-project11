d385d924809cf88e04d3f40962a3fd3e83e66992
Pandurang Pawar <121596184+pandurang6662@users.noreply.github.com>
2023-03-17 16:53:27 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resoucre "aws_instance" "key" {
 ami = "ami-02f3f602d23f1659d"
 instance_type = "t2.micro"
-
+vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tages = {
 Name = "project-instance"
 Environment = "prod"
